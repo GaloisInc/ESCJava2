fcb876bb8eaa61b29bafe2b78083fb287bd9d3dc
nobody <nobody@localhost>
2003-01-06 11:59:09 +0000

    This commit was manufactured by cvs2svn to create branch 'sos'.

--- ESCTools/Javafe/java/javafe/parser/FileFormatException.java
+++
@@ -1,7 +0,0 @@
-
-package javafe.parser;
-
-public class FileFormatException extends java.io.IOException
-{
-  public FileFormatException(String msg) { super(msg); }
-}
--- ESCTools/Javafe/java/javafe/util/AssertionFailureException.java
+++
@@ -1,9 +0,0 @@
-/* Copyright 2000, 2001, Compaq Computer Corporation */
-
-
-package javafe.util;
-
-public class AssertionFailureException extends RuntimeException {
-  AssertionFailureException() { }
-  AssertionFailureException(String msg) { super(msg); }
-}
