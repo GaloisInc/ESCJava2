105cb6a73008f97d81666a7e54be9e601de1bbfc
nobody <nobody@localhost>
2007-04-30 19:51:44 +0000

    This commit was manufactured by cvs2svn to create branch 'BCEL'.

--- MobiusDirectVCGen/logic/bicolano-jar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-cd Formalisation; make; cd ..
-jar cvf bicolano.jar `find ./Formalisation/Bicolano -name "*.v"` \
-                     `find ./Formalisation/Bicolano -name "*.vo"` \
-                     `find ./Formalisation/Library -name "*.v"` \
-                     `find ./Formalisation/Library -name "*.vo"`  defs_types.v
